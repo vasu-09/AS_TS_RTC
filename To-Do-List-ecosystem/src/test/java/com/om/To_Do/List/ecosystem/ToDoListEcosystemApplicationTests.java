959054532cb04595328c2b856133f102dad55b50
vasu-09 <87554837+vasu-09@users.noreply.github.com>
2025-10-01 14:17:56 +0530

    Update ToDoListEcosystemApplicationTests.java

--- To-Do-List-ecosystem/src/test/java/com/om/To_Do/List/ecosystem/ToDoListEcosystemApplicationTests.java
+++ To-Do-List-ecosystem/src/test/java/com/om/To_Do/List/ecosystem/ToDoListEcosystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.om.To_Do.List.ecosystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToDoListEcosystemApplicationTests {
 
 	@Test
