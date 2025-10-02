314f03b2896e2f9114729a582d0e9ff8cb14edfc
srinivas-01 <62439147+srinivas-01@users.noreply.github.com>
2025-10-02 10:36:59 +0530

    atjier1

--- Notification-Service/src/test/java/com/om/Notification_Service/NotificationServiceApplicationTests.java
+++ Notification-Service/src/test/java/com/om/Notification_Service/NotificationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.om.Notification_Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
 
 	@Test
