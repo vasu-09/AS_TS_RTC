304791416fb23efedbd15a769dcd9356d9efaa8d
srinivas-01 <62439147+srinivas-01@users.noreply.github.com>
2025-10-02 23:25:18 +0530

    adsi

--- Notification-Service/src/test/java/com/om/Notification_Service/NotificationServiceApplicationTests.java
+++ Notification-Service/src/test/java/com/om/Notification_Service/NotificationServiceApplicationTests.java
@@ -2,10 +2,12 @@ package com.om.Notification_Service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)	
 class NotificationServiceApplicationTests {
 
 	@Test
